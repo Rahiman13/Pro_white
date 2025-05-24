fba778c37cd21809be7057719c5384c0fe27278c
Rahiman Shaik <rahimanshaik13@gmail.com>
2025-05-24 20:17:31 +0530

    staffing png rename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ✅ Important for production
   plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, // This is needed!
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
